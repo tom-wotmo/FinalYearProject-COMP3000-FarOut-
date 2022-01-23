54bd7e7c4213f12ee67cbc7aca4411dcee7c8a92
tom-wotmo <57358973+tom-wotmo@users.noreply.github.com>
2022-01-23 20:19:01 +0000

    Adding depth to the map surrrounding

--- FarOut/Assets/Scripts/NPC/InteractableNPC.cs
+++ FarOut/Assets/Scripts/NPC/InteractableNPC.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class InteractableNPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public GameObject NpcInteract;
+    public GameObject NpcIcon;
+  
+    private void OnTriggerEnter(Collider other)
     {
         
     }
